abef8e45046588fba542290890c468e028831258
Dante <francescopolymathes@gmail.com>
2024-01-15 20:50:08 +0100

    Update

--- LSTMbot_highAcc.py
+++ LSTMbot_highAcc.py
@@ -56,7 +56,6 @@ def create_lstm_model(X_train, y_train, data_set_points):
     inputs = Dropout(0.1, name='dropout_0')(inputs)
     inputs = LSTM(32, name='lstm_1')(inputs)
     inputs = Dropout(0.05, name='dropout_1')(inputs)
-    # Add a dense layer with 32 units and a linear activation function
     inputs = Dense(32, name='dense_0')(inputs)
     # Add another dense layer with 1 unit and a linear activation function
     inputs = Dense(1, name='dense_1')(inputs)
